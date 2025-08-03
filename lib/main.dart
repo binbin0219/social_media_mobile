453f0b6641287d9c3c28c034c1e39ecc4be66676
binbin0219 <148597883+binbin0219@users.noreply.github.com>
2025-08-03 17:35:09 +0800

    Added login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:social_media_mobile/screens/home_page.dart';
 import 'package:social_media_mobile/screens/login_page.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(); // Load the .env file
   runApp(const MyApp());
 }
 
@@ -21,78 +21,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const LoginPage(),
-        '/home': (context) => const MyHomePage(title: "Home"),
+        '/home': (context) => const HomePage(),
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  dynamic _fetchedData = "";
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  Future<dynamic> _fetchDataFromApi(String url) async {
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode != 200) {
-      throw Error();
-    }
-
-    final data = jsonDecode(response.body);
-    return data;
-  }
-
-  void _handlePress() async {
-    try {
-      final fetchedData = await _fetchDataFromApi(
-        'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata',
-      );
-      setState(() {
-        _fetchedData = fetchedData;
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_fetchedData',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _handlePress,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,96 +12,173 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+
+  void _submitForm() async {
+    try {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+        bool success = await loginUser(_email, _password);
+
+        if (success) {
+          Navigator.pushNamed(context, "/home");
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Invalid email or password")),
+          );
+        }
+      }
+    } catch (e) {
+      print("Failed to login: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Something went wrong, please try again."),
+        ),
+      );
+    }
+  }
+
+  Future<bool> loginUser(String email, String password) async {
+    final response = await http.post(
+      Uri.parse("${dotenv.env['BACK_END']}/api/auth/login"),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+    print(response.statusCode);
+
+    return response.statusCode == 200;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF5F6FA),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-          child: Column(
-            children: [
-              const Text(
-                "Blogify",
-                style: TextStyle(
-                  fontSize: 45,
-                  color: Colors.deepPurple,
-                  fontWeight: FontWeight.w900,
-                  fontFamily: 'FugazOne',
-                ),
-              ),
-              const SizedBox(height: 30),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Image.asset('assets/images/login-bg.jpg', fit: BoxFit.cover),
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                children: [
+                  const Text(
+                    "Blogify",
+                    style: TextStyle(
+                      fontSize: 45,
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: 'FugazOne',
+                    ),
+                  ),
+                  const SizedBox(height: 30),
 
-              Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 8,
-                            offset: Offset(0, 4),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 8,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: 'Email',
-                          prefixIcon: Icon(Icons.email),
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.all(18),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 14),
-
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 8,
-                            offset: Offset(0, 4),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'Email',
+                              prefixIcon: Icon(Icons.email),
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.all(18),
+                            ),
+                            onSaved: (value) => _email = value!,
+                            validator:
+                                (value) =>
+                                    value == null || value.isEmpty
+                                        ? 'Enter email'
+                                        : null,
                           ),
-                        ],
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.lock),
-                          contentPadding: EdgeInsets.all(18),
                         ),
-                        obscureText: true,
-                      ),
-                    ),
-                    const SizedBox(height: 30),
+                        const SizedBox(height: 14),
 
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 14),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 8,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              border: InputBorder.none,
+                              prefixIcon: Icon(Icons.lock),
+                              contentPadding: EdgeInsets.all(18),
+                            ),
+                            obscureText: true,
+                            onSaved: (value) => _password = value!,
+                            validator:
+                                (value) =>
+                                    value == null || value.isEmpty
+                                        ? 'Enter email'
+                                        : null,
+                          ),
                         ),
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(fontSize: 16),
+                        const SizedBox(height: 30),
+
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: _submitForm,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.deepPurple,
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                        const SizedBox(height: 20),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("Don't have an account?"),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("Sign Up"),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
